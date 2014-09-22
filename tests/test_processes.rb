f8226e98d4b6e58ea7c9328634b71bdd208a6935
Thomas Haggett <thomas@haggett.org>
2014-09-22 16:21:22 +0100

    Use bash rather than sh which emits it’s version information out via stderr.

--- tests/test_processes.rb
+++ tests/test_processes.rb
@@ -3,7 +3,7 @@
 # Author:: Francis Cianfrocca (gmail: blackhedd)
 # Homepage::  http://rubyeventmachine.com
 # Date:: 8 April 2006
-# 
+#
 # See EventMachine and EventMachine::Connection for documentation and
 # usage examples.
 #
@@ -11,12 +11,12 @@
 #
 # Copyright (C) 2006-07 by Francis Cianfrocca. All Rights Reserved.
 # Gmail: blackhedd
-# 
+#
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of either: 1) the GNU General Public License
 # as published by the Free Software Foundation; either version 2 of the
 # License, or (at your option) any later version; or 2) Ruby's License.
-# 
+#
 # See the file COPYING for complete licensing information.
 #
 #---------------------------------------------------------------------------
@@ -104,7 +104,7 @@ class TestProcesses < Test::Unit::TestCase
 
   def test_em_system_cmd_arguments
     EM.run{
-      EM.system('sh', '--version', proc{ |process|
+      EM.system('bash', '--version', proc{ |process|
       }, proc{ |out,status|
         $out = out
         $status = status
